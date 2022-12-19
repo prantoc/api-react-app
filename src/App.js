2e8ae32eb449651ba8fc27c068e812c35e6f4c29
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-12-19 23:29:38 +0600

    showing users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from 'react-bootstrap';
-import UserTable from './components/UserTable/UserTable';
+import User from './components/User/User';
+// import UserTable from './components/UserTable/UserTable';
 
 function App() {
   return (
@@ -7,7 +8,8 @@ function App() {
       <h1>Users Table</h1>
       <Row>
         <Col md={12}>
-          <UserTable />
+          {/* <UserTable /> */}
+          <User></User>
         </Col>
       </Row>
 
